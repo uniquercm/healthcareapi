e1e7160dd3ee4a30efc25f62ffe9163438a188d7
uniquercm <84480377+uniquercm@users.noreply.github.com>
2021-05-28 02:45:19 -0700

    Add files via upload

---
+++ Web.Api.Infrastructure/Data/Repositories/HealthRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Web.Api.Core.Interfaces.Gateways.Repositories;
+using Dapper;
+
+namespace Web.Api.Infrastructure.Data.Repositories
+{
+    internal sealed class HealthRepository : IHealthRepository
+    {
+        private readonly AppDbContext _appDbContext;
+        public HealthRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        /// <summary>
+        /// Check the database health
+        /// </summary>
+        /// <returns>true</returns>
+        public async Task<bool> CheckDBHealth()
+        {
+            using (var connection = _appDbContext.Connection)
+            {
+                var sqlQuery = $"select 5";
+                Int16 sql = await connection.QuerySingleOrDefaultAsync<Int16>(sqlQuery);
+                return sql.Equals(5) ? true : false;
+            }
+
+
+        }
+    }
+}
