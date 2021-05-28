b6ddd71cda4fa0b2aa4aaf04230a1d01d02da7c1
uniquercm <84480377+uniquercm@users.noreply.github.com>
2021-05-28 03:14:45 -0700

    Add files via upload

---
+++ Web.Api/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace Web.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new HostBuilder()
+                            .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                            .ConfigureWebHostDefaults(webBuilder =>
+                            {
+                                webBuilder
+                                .UseContentRoot(Directory.GetCurrentDirectory())
+                                .UseIISIntegration()
+                                .UseUrls("http://0.0.0.0:2003")
+                                //.UseUrls("http://0.0.0.0")
+                                //.UseUrls("https://api.uniquercm.org")
+                                .UseStartup<Startup>();
+                            })
+                            .Build();
+            host.Run();
+        }
+    }
+}
